7770235fe3c1188d47a353adbbe11a97067b301b
Yaseen k <yzac1569@gmail.com>
2025-02-18 13:01:55 +0530

    Update record1.sql

--- record1.sql
+++ record1.sql
@@ -1,3 +1,7 @@
+#DDL-CREATE  
+
+1.      Create a table EMPLOYEE have the following fields (emp_id, name,city,dob,adhar_no)
+
 mysql> CREATE TABLE EMPLOYEE(emp_id INT, name VARCHAR(20), city VARCHAR(50), dob DATE, adhar_no INT);
 Query OK, 0 rows affected (0.63 sec)
 
@@ -12,6 +16,17 @@ mysql> DESCRIBE EMPLOYEE;
 | adhar_no | int         | YES  |     | NULL    |       |
 +----------+-------------+------+-----+---------+-------+
 5 rows in set (0.00 sec)
+
+
+2.      Create a table STUDENT have the fields(roll_no, name, age, address, gender)and add following constraints on it.
+
+         Roll_no – Primary key
+
+         Name – does not allow null values
+
+         Age- must be in between (0-99), does not allow null values
+
+         Gender- default value is ‘m’
 
 mysql> CREATE TABLE Customer (id INT, name VARCHAR(20), address VARCHAR(50), city VARCHAR(50), phone_no INT);
 Query OK, 0 rows affected (1.42 sec)
@@ -30,6 +45,9 @@ mysql> DESCRIBE STUDENT;
 +---------+-------------+------+-----+---------+-------+
 4 rows in set (0.00 sec)
 
+3.      Create a temporary table STUDENT1 of STUDENT relation.
+
+
 mysql> CREATE TEMPORARY TABLE STUDENT1 AS SELECT * FROM STUDENT;
 Query OK, 0 rows affected (0.01 sec)
 Records: 0  Duplicates: 0  Warnings: 0
@@ -45,6 +63,7 @@ mysql> DESCRIBE STUDENT1;
 +---------+-------------+------+-----+---------+-------+
 4 rows in set (0.00 sec)
 
+4.      Create a duplicate table of EMPLOYEE.
 mysql> CREATE TABLE d_emp AS SELECT * FROM EMPLOYEE;
 Query OK, 0 rows affected (0.70 sec)
 Records: 0  Duplicates: 0  Warnings: 0
