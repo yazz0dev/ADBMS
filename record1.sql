f788b554e32f58becb3c9a752c4d2d68fefd1d52
Yaseen k <yzac1569@gmail.com>
2025-02-18 13:04:44 +0530

    COMPLETED

--- record1.sql
+++ record1.sql
@@ -1,6 +1,5 @@
-#DDL-CREATE  
-
-1.      Create a table EMPLOYEE have the following fields (emp_id, name,city,dob,adhar_no)
+# DDL-CREATE
+##1.      Create a table EMPLOYEE have the following fields (emp_id, name,city,dob,adhar_no)
 
 mysql> CREATE TABLE EMPLOYEE(emp_id INT, name VARCHAR(20), city VARCHAR(50), dob DATE, adhar_no INT);
 Query OK, 0 rows affected (0.63 sec)
@@ -18,7 +17,7 @@ mysql> DESCRIBE EMPLOYEE;
 5 rows in set (0.00 sec)
 
 
-2.      Create a table STUDENT have the fields(roll_no, name, age, address, gender)and add following constraints on it.
+##2.      Create a table STUDENT have the fields(roll_no, name, age, address, gender)and add following constraints on it.
 
          Roll_no – Primary key
 
@@ -45,8 +44,8 @@ mysql> DESCRIBE STUDENT;
 +---------+-------------+------+-----+---------+-------+
 4 rows in set (0.00 sec)
 
-3.      Create a temporary table STUDENT1 of STUDENT relation.
-
+         
+##3.      Create a temporary table STUDENT1 of STUDENT relation.
 
 mysql> CREATE TEMPORARY TABLE STUDENT1 AS SELECT * FROM STUDENT;
 Query OK, 0 rows affected (0.01 sec)
@@ -63,7 +62,9 @@ mysql> DESCRIBE STUDENT1;
 +---------+-------------+------+-----+---------+-------+
 4 rows in set (0.00 sec)
 
-4.      Create a duplicate table of EMPLOYEE.
+         
+##4.      Create a duplicate table of EMPLOYEE.
+         
 mysql> CREATE TABLE d_emp AS SELECT * FROM EMPLOYEE;
 Query OK, 0 rows affected (0.70 sec)
 Records: 0  Duplicates: 0  Warnings: 0
