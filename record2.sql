d7f1f18810146b425d37b35df65bb022649a6f5e
Yaseen k <yzac1569@gmail.com>
2025-03-04 15:00:14 +0530

    COMPLETED

--- record2.sql
+++ record2.sql
@@ -1,8 +1,8 @@
 #2.  DML COMMANDS
 
-  a)  DML -INSERT
-
-                     Insert values in INVOICE database. 
+  '''a)  *DML -INSERT
+  
+Insert values in INVOICE database. 
 
 Value insertion................
 -> customer_id  10001, 10002, 10003 etc
@@ -13,7 +13,7 @@ Value insertion................
 -> some phone number should be null
 -> some price between 50 rs and 150 rs
 -> inv_date between 1st jan 2023 to 31st dec 2024
--> some customers purchase more than 2 products#
+-> some customers purchase more than 2 products'''
 
 mysql> insert into customer values(1001, 'arun' , 'mukkam' ,'calicut', 987654321);
 Query OK, 1 row affected (0.12 sec)
@@ -98,15 +98,13 @@ mysql> select * from Invoice_item;
 
 
 
-
-
-#b) DML - UPDATE 
+'''b) DML - UPDATE 
 
               1. Update the price of a particular product. 
               2. Change the name of city ‘calicut’ to ‘kozhikode’ in the customer table. 
               3. Update all prices with 2% of its original price. 
               4. Change the address, city, phone number of a particular customer. 
-              5. Change the quantity of a purchased product of inv_id ‘101’#
+              5. Change the quantity of a purchased product of inv_id ‘101’'''
 
 
 
@@ -137,90 +135,45 @@ mysql> select * from product;
 3 rows in set (0.00 sec)
 
 
-#c) DML - DELETE 
-
-#1. Delete the record of customer id ‘10001’. 
-              
-         
-#2. Delete all purchase details of items its quantity less than 2. 
-              mysql> DELETE FROM Invoice_item WHERE quantity < 2;
-Query OK, 1 row affected (0.10 sec)
-
-mysql> select * from Invoice_item;
-+--------+------+----------+
-| inv_id | pid  | quantity |
-+--------+------+----------+
-|    101 |    2 |        5 |
-|    102 |    3 |        5 |
-|    103 |    1 |        2 |
-+--------+------+----------+
-3 rows in set (0.00 sec)
-
+##c) DML - DELETE 
 
 #3. Delete the purchase details of product ‘pen’ in the in_id ‘101’. 
+  
+mysql> DELETE FROM Invoice_item WHERE inv_id=101;
+Query OK, 1 row affected (0.13 sec)
 
- mysql> DELETE FROM Invoice_item WHERE inv_id = 101 AND pid = (SELECT pid FROM product WHERE pname = 'pen');
-Query OK, 0 rows affected (0.02 sec)
-
-mysql> SELECT * FROM Invoice_item;
-+--------+------+----------+
-| inv_id | pid  | quantity |
-+--------+------+----------+
-|    101 |    2 |        5 |
-|    102 |    3 |        5 |
-|    103 |    1 |        2 |
-+--------+------+----------+
-3 rows in set (0.00 sec)
-
-#4. Delete details of employees who does not have phone number. 
-        
-        
-#5. Delete Invoice details of a particular customer(name) 
-              mysql> DELETE FROM Invoice_item
-    -> WHERE inv_id IN (SELECT inv_id FROM Invoice_master WHERE cust_id IN (SELECT id FROM customer WHERE name = 'ajay'));
-Query OK, 1 row affected (0.12 sec)
-
-mysql> DELETE FROM Invoice_master
-    -> WHERE cust_id IN (SELECT id FROM customer WHERE name = 'ajay');
+  
+#1. Delete the record of customer id ‘10001’. 
+  
+mysql> DELETE FROM Invoice_item WHERE inv_id=101;
+Query OK, 1 row affected (0.11 sec)
+mysql> DELETE FROM Invoice_master WHERE inv_id=101;
+Query OK, 1 row affected (0.06 sec)
+mysql> DELETE FROM customer WHERE id=10001;
 Query OK, 1 row affected (0.11 sec)
 
-mysql> SELECT * FROM Invoice_master;
-+--------+---------+------------+
-| inv_id | cust_id | inv_date   |
-+--------+---------+------------+
-|    101 |    1001 | 2023-02-25 |
-|    103 |    1003 | 2024-01-22 |
-+--------+---------+------------+
-2 rows in set (0.00 sec)
+  
+#2. Delete all purchase details of items its quantity less than 2. 
+  
+mysql> DELETE FROM Invoice_item WHERE quantity < 2;
+Query OK, 1 row affected (0.14 sec)
 
-mysql> SELECT * FROM Invoice_item;
-+--------+------+----------+
-| inv_id | pid  | quantity |
-+--------+------+----------+
-|    101 |    2 |        5 |
-|    103 |    1 |        2 |
-+--------+------+----------+
-2 rows in set (0.00 sec)
+  
+#4. Delete details of customer who does not have phone number. 
+  
+mysql> DELETE FROM EMPLOYEE WHERE phno IS NULL;
+Query OK, 1 row affected (0.10 sec)
 
+  
+#5. Delete Invoice details of a particular customer(name) 
+  
+mysql> DELETE FROM Invoice_item WHERE inv_id=103;
+Query OK, 0 rows affected (0.12 sec)
 
-              
-#6. Delete the first two records of INVOICE_ITEM table its purchase quantity is high.#
-mysql> CREATE TEMPORARY TABLE TempToDelete AS
-    -> SELECT inv_id, pid
-    -> FROM Invoice_item
-    -> ORDER BY quantity DESC
-    -> LIMIT 2;
-Query OK, 2 rows affected (0.00 sec)
-Records: 2  Duplicates: 0  Warnings: 0
-
-mysql> DELETE FROM Invoice_item
-    -> WHERE (inv_id, pid) IN (SELECT inv_id, pid FROM TempToDelete);
-Query OK, 2 rows affected (0.13 sec)
-
-mysql> DROP TEMPORARY TABLE TempToDelete;
-Query OK, 0 rows affected (0.06 sec)
-
-mysql> SELECT * FROM Invoice_item;
-Empty set (0.00 sec)
+  
+#6. Delete the first two records of INVOICE_ITEM table.
+  
+mysql> DELETE FROM Invoice_item LIMIT 2;
+Query OK, 0 rows affected (0.02 sec)
 
 
